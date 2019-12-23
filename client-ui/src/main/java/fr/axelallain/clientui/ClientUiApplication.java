973f60bbfb467656daf293ac71cea871841e5791
Axel Allain <axel.allain.a@gmail.com>
2019-12-23 07:50:47 +0100

    Adding 'eureka' edge microservice - Connecting microservices to eureka server

--- client-ui/src/main/java/fr/axelallain/clientui/ClientUiApplication.java
+++ client-ui/src/main/java/fr/axelallain/clientui/ClientUiApplication.java
@@ -2,10 +2,12 @@ package fr.axelallain.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("fr.axelallain.clientui")
+@EnableDiscoveryClient
 public class ClientUiApplication {
 
 	public static void main(String[] args) {
