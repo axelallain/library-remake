3de5766af2c98df425e6aa6104ecd9934f47dcef
Axel Allain <axel.allain.a@gmail.com>
2020-01-18 23:09:43 +0100

    Fix 403 Forbidden error for the user signup method - BCrypt has been added for user signup - Adding Spring Security config class for disabling CSRF

---
+++ users/src/main/java/fr/axelallain/users/dao/UsersDao.java
@@ -0,0 +1,9 @@
+package fr.axelallain.users.dao;
+
+import fr.axelallain.users.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersDao extends JpaRepository<User, Integer> {
+
+
+}
